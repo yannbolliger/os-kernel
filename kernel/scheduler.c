6cec8d8d9c71a7e0d62842cd1e10ac07843d8fb1
Yann Bolliger <ybstudio@windowslive.com>
2018-03-28 13:43:42 +0200

    Rewrite scheduler for pcb table use and round robin.

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -8,8 +8,7 @@ extern void main_P4();
 extern uint32_t tos_P3;
 extern uint32_t tos_P4;
 
-pcb_t pcb[2];
-int executing = 0;
+pcb_table_t pcb_table = {0};
 
 /**
  * Initialise PCBs representing processes stemming from execution of
@@ -22,44 +21,56 @@ int executing = 0;
 
 void scheduler_rst(ctx_t* ctx) {
 
-  memset(&pcb[0], 0, sizeof(pcb_t));
-  pcb[0].pid      = 1;
-  pcb[0].status   = STATUS_READY;
-  pcb[0].ctx.cpsr = 0x50;
-  pcb[0].ctx.pc   = (uint32_t) (&main_P3);
-  pcb[0].ctx.sp   = (uint32_t) (&tos_P3);
+  // reset PCB table
+  memset(&pcb_table, 0, sizeof(pcb_table_t));
 
-  memset(&pcb[1], 0, sizeof(pcb_t));
-  pcb[1].pid      = 2;
-  pcb[1].status   = STATUS_READY;
-  pcb[1].ctx.cpsr = 0x50;
-  pcb[1].ctx.pc   = (uint32_t) (&main_P4);
-  pcb[1].ctx.sp   = (uint32_t) (&tos_P4);
+  // populate with two processes
+  pcb_table.pcb[0].pid      = 1;
+  pcb_table.pcb[0].status   = STATUS_READY;
+  pcb_table.pcb[0].ctx.cpsr = 0x50;
+  pcb_table.pcb[0].ctx.pc   = (uint32_t) (&main_P3);
+  pcb_table.pcb[0].ctx.sp   = (uint32_t) (&tos_P3);
 
+  pcb_table.pcb[1].pid      = 2;
+  pcb_table.pcb[1].status   = STATUS_READY;
+  pcb_table.pcb[1].ctx.cpsr = 0x50;
+  pcb_table.pcb[1].ctx.pc   = (uint32_t) (&main_P4);
+  pcb_table.pcb[1].ctx.sp   = (uint32_t) (&tos_P4);
 
-  memcpy(ctx, &pcb[0].ctx, sizeof(ctx_t));
-  pcb[0].status = STATUS_EXECUTING;
-  executing = 0;
+  pcb_table.pcb_size = 2;
+
+  // set first process to executing state
+  memcpy(ctx, &pcb_table.pcb[0].ctx, sizeof(ctx_t));
+  pcb_table.pcb[0].status = STATUS_EXECUTING;
+  pcb_table.executing_pid = pcb_table.pcb[0].pid;
+
+  return;
+}
+
+void interrupt_process(pid_t pid, ctx_t* ctx) {
+  pcb_t* interrupted = &pcb_table.pcb[pid];
+
+  memcpy(&interrupted->ctx, ctx, sizeof(ctx_t));
+  interrupted->status = STATUS_READY;
+
+  return;
+}
+
+void dispatch_process(pid_t pid, ctx_t* ctx) {
+  pcb_t* dispatched = &pcb_table.pcb[pid];
+
+  memcpy(ctx, &dispatched->ctx, sizeof(ctx_t));
+  dispatched->status = STATUS_EXECUTING;
 
   return;
 }
 
 void scheduler(ctx_t* ctx) {
 
-  if (0 == executing) {
-    memcpy(&pcb[0].ctx, ctx, sizeof(ctx_t));       // preserve P_1
-    pcb[0].status = STATUS_READY;                  // update   P_1 status
-    memcpy(ctx, &pcb[1].ctx, sizeof(ctx_t));       // restore  P_2
-    pcb[1].status = STATUS_EXECUTING;              // update   P_2 status
-    executing = 1;                                 // update   index => P_2
-  }
-  else if (1 == executing) {
-    memcpy(&pcb[1].ctx, ctx, sizeof(ctx_t));       // preserve P_2
-    pcb[1].status = STATUS_READY;                  // update   P_2 status
-    memcpy(ctx, &pcb[0].ctx, sizeof(ctx_t));       // restore  P_1
-    pcb[0].status = STATUS_EXECUTING;              // update   P_1 status
-    executing = 0;                                 // update   index => P_1
-  }
+  interrupt_process(pcb_table.executing_pid, ctx);
+
+  pcb_table.executing_pid = (pcb_table.executing_pid + 1) % pcb_table.pcb_size;
+  dispatch_process(pcb_table.executing_pid, ctx);
 
   return;
 }
