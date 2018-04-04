6f7e0f7f84d4dbfe1e5398682734877adbbc2b13
Yann Bolliger <ybstudio@windowslive.com>
2018-04-04 12:14:18 +0200

    Add memory boundaries to linker script and code.

--- kernel/mem.h
+++ kernel/mem.h
@@ -3,12 +3,17 @@
 #ifndef __MEM_H
 #define __MEM_H
 
+#include <string.h>
 #include "kernel.h"
 
-extern uint32_t MEM_LO;
-extern uint32_t MEM_HI;
+/**
+ * Memory boundaries (MUST ALSO UPDATE LINKER SCRIPT!)
+ */
+#define MEM_LO 0x70020000
+#define MEM_HI 0x90000000
 
 #define MEM_BLOCK_SIZE (0x1000)
+#define MEM_BLOCK_NUMBER ((MEM_HI - MEM_LO)/(MEM_BLOCK_SIZE))
 
 uint32_t mem_allocate(const size_t n_blocks);
 
@@ -17,4 +22,4 @@ size_t mem_deallocate(const uint32_t address, const size_t n_blocks);
 size_t mem_copy(const uint32_t src_addr, const uint32_t dst_addr,
     const size_t n_blocks);
 
-#endif
+ #endif
