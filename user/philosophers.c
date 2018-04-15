2c98e9c823f704d0d5cbbb3a3e2ca30bf908263a
Yann Bolliger <yann.bolliger@epfl.ch>
2018-04-15 23:35:07 +0200

    Correct id and better/faster random pseudo sleep.

--- user/philosophers.c
+++ user/philosophers.c
@@ -12,11 +12,6 @@ int fork_pipes[PHIL_NUMBER * 2];
 int fork_pipes_read[2];
 int fork_pipes_write[2];
 
-/**
- * ID of a particular philosopher.
- */
-int id;
-
 #define PHIL_NAME_LENGTH (11)
 const char* names[PHIL_NUMBER] = {
   "Socrates   ",
@@ -37,7 +32,7 @@ const char* names[PHIL_NUMBER] = {
   "Russel     ",
 };
 
-const char* name() {
+const char* name(const size_t id) {
   return names[id];
 }
 
@@ -60,14 +55,13 @@ const char* topics[TOPIC_NUMBER] = {
 
 #define RAND_LENGTH (20)
 const int rand_int[RAND_LENGTH] = {
-  7, 33, 249, 48, 178, 8, 168, 158, 99, 45, 8, 245, 92, 34, 51, 151,
-  215, 1, 72, 116
+  61, 7, 38, 2, 50, 5, 43, 9, 47, 18, 26, 2, 57, 18, 21, 38, 49, 37, 28, 44
 };
 
 int rand_index;
-int rand_next() {
+int rand_next(const size_t id) {
   rand_index = (rand_index + id) % RAND_LENGTH;
-  return rand_int[rand_index] % 256;
+  return rand_int[rand_index];
 }
 
 /**
@@ -78,7 +72,7 @@ int rand_next() {
  * The philosophers always pick up their primary frok first. In that way the
  * forks are always aquired in the same order by all the philosophers.
  */
-void close_all_except_own_fork_pipes() {
+void close_all_except_own_fork_pipes(const size_t id) {
   // write to own fork e.g. put own fork on the table
   write(fork_pipes[id*2 + 1], "X", 1);
 
@@ -99,7 +93,7 @@ void close_all_except_own_fork_pipes() {
   }
 
   // close all other fds
-  for (int i = 0; i < PHIL_NUMBER; i++) {
+  for (size_t i = 0; i < PHIL_NUMBER; i++) {
     if (i != id && i != (id + 1) % PHIL_NUMBER) {
       //close(fork_pipes[2*i]);
       //close(fork_pipes[2*i + 1]);
@@ -108,19 +102,20 @@ void close_all_except_own_fork_pipes() {
 }
 
 void pseudo_wait(const size_t duration) {
-  for (size_t i = 0; i < (duration << 20); i++) {
+  for (size_t i = 0; i < (duration << 16); i++) {
     double a = duration * 555.555;
     double b = i * 77.7777;
     size_t c = a * b;
   }
+  yield();
 }
 
-void think() {
-  pseudo_wait(rand_next());
+void think(const size_t id) {
+  pseudo_wait(rand_next(id));
 
-  const char* topic = topics[rand_next() % TOPIC_NUMBER];
+  const char* topic = topics[rand_next(id) % TOPIC_NUMBER];
 
-  write(STDOUT_FILENO, name(), PHIL_NAME_LENGTH);
+  write(STDOUT_FILENO, name(id), PHIL_NAME_LENGTH);
   write(STDOUT_FILENO, " thinks about ", 14);
   write(STDOUT_FILENO, topic, TOPIC_LENGTH);
   write(STDOUT_FILENO, "\n", 1);
@@ -141,34 +136,33 @@ void put_down(const fork_t fork) {
   write(fork_pipes_write[fork], "X", 1);
 }
 
-void eat() {
+void eat(const size_t id) {
 
   while (!picked_up(FIRST_FORK)) { /* busily wait */ };
-  write(STDOUT_FILENO, name(), PHIL_NAME_LENGTH);
+  write(STDOUT_FILENO, name(id), PHIL_NAME_LENGTH);
   write(STDOUT_FILENO, " has one fork.\n", 15);
 
   while (!picked_up(SECOND_FORK)) { /* busily wait */ };
-  write(STDOUT_FILENO, name(), PHIL_NAME_LENGTH);
+  write(STDOUT_FILENO, name(id), PHIL_NAME_LENGTH);
   write(STDOUT_FILENO, " has both forks and starts to eat!\n", 35);
 
-  pseudo_wait(rand_next());
+  pseudo_wait(rand_next(id));
 
-  write(STDOUT_FILENO, name(), PHIL_NAME_LENGTH);
+  write(STDOUT_FILENO, name(id), PHIL_NAME_LENGTH);
   write(STDOUT_FILENO, " releases its two forks.\n", 25);
   put_down(FIRST_FORK);
   put_down(SECOND_FORK);
 }
 
-void philosophize(const int p_id) {
-  id = p_id;
-  close_all_except_own_fork_pipes();
+void philosophize(const size_t id) {
+  close_all_except_own_fork_pipes(id);
 
   for (size_t i = 0; i < ROUNDS; i++) {
-    think();
-    eat();
+    think(id);
+    eat(id);
   }
 
-  write(STDOUT_FILENO, name(), PHIL_NAME_LENGTH);
+  write(STDOUT_FILENO, name(id), PHIL_NAME_LENGTH);
   write(STDOUT_FILENO, " is done with philosophizing.\n", 30);
   exit(EXIT_SUCCESS);
 }
